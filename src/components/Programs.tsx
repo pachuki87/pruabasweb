1ec52e38f716ef5f2f98c34a1c41b245fa6c0795
pachuki87 <pablocardonafelu@gmail.com>
2025-09-07 00:02:21 +0200

    Reorganizar layout de cursos para mejor distribución del espacio

--- src/components/Programs.tsx
+++ src/components/Programs.tsx
@@ -42,7 +42,7 @@ const Programs = () => {
           </div>
           
           <div className="lg:col-span-2">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
               {programs.map((program, index) => (
                 <a
                   key={index}
